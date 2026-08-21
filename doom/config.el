6426bc2b5eca48116542f043f282473e19f54e9f
Elijah Zimmerman <elijahz0409@gmail.com>
2026-08-21 16:33:46 -0400

    Update i3blocks style and Doom splash

--- doom/config.el
+++ doom/config.el
@@ -3,11 +3,19 @@
 
 (setq projectile-project-search-path `("~/Programs"))
 
-(setq fancy-splash-image "~/.config/doom/st-ephrael-banner.png")
+(setq fancy-splash-image "~/.config/doom/st-ephrael-noback.png")
 (add-to-list 'custom-theme-load-path "~/.config/doom/themes/")
 (setq doom-theme `horizon-trash-panda)
 
 (setq display-line-numbers-type t)
+
+;; Emacs is launched via i3 `exec`, which doesn't source ~/.profile, so
+;; ~/go/bin (gopls, gore, etc.) never makes it into Emacs's exec-path.
+(let ((go-bin (expand-file-name "~/go/bin")))
+  (when (and (file-directory-p go-bin)
+             (not (member go-bin exec-path)))
+    (add-to-list 'exec-path go-bin)
+    (setenv "PATH" (concat go-bin path-separator (getenv "PATH")))))
 
 (after! org
   (org-babel-do-load-languages
@@ -16,3 +24,5 @@
            '((mermaid . t))))
   (setq ob-mermaid-cli-path (executable-find "mmdc")))
 
+(after! lsp-mode
+  (setq lsp-enable-snippet nil))
